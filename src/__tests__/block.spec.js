55815b08affa63d9b52d58a33695bb26a3c46f10
Giancarlo Sanchez <gianksp@gmail.com>
2023-04-27 22:18:37 +0200

    New style

--- src/__tests__/block.spec.js
+++ src/__tests__/block.spec.js
@@ -6,7 +6,7 @@ describe("Toast block", () => {
         it("Has id, label, category and content set", async () => {
             expect(block.id).toEqual(`section-${process.env.MODULE_ID}`)
             expect(block.label).toBeTruthy()
-            expect(block.category).toEqual('Notifications')
+            expect(block.category).toEqual('Toast')
             expect(block.content).toBeTruthy()
         })
     })
