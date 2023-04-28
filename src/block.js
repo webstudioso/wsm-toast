89362e57cf1b1591bd7a9d0f5ffab37e027c97cf
Giancarlo Sanchez <gianksp@gmail.com>
2023-04-28 09:49:08 +0200

    Modernized

--- src/block.js
+++ src/block.js
@@ -1,14 +1,14 @@
 const block = {
   id: `section-${process.env.MODULE_ID}`,
   media: `
-    <div class="px-5 py-4 border-r-8 drop-shadow-lg success text-white border-green-600 bg-green-600">
+    <a class="relative px-5 py-4 border-r-8 bg-white drop-shadow-lg border-green-600">
       <p class="text-sm">
-          Notification toast!
+          Notification!
       </p>
-    </div>
+    </a>
   `,
   category: "Toast",
-  label: 'Event notifications',
+  label: 'Visible during events',
   content: `
     <div id="${process.env.MODULE_ID}" class="hidden fixed right-10 bottom-10 px-5 py-4 border-r-8 bg-white drop-shadow-lg">
         <p class="text-sm">
